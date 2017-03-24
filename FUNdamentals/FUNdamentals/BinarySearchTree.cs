a3a5e896445f62a9be5241f78a0dc256b654010e
Chase Violet <ccviol01@gmail.com>
2017-03-23 18:50:04 -0600

    cool first commit

---
+++ FUNdamentals/FUNdamentals/BinarySearchTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeQuestions
+{
+    public class BinarySearchTree<T> where T : IComparable
+    {
+        private Node rootNode = null;
+        public void Insert(T value)
+        {
+            if (rootNode == null)
+                rootNode = new Node(value);
+
+            bool IsLeft = value.CompareTo(rootNode.Value) <= 0;
+
+            if(IsLeft)
+            {
+
+            }
+        }
+
+
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+            public T Value;
+            public Node<T> Left;
+            public Node<T> Right;
+        }
+    }
+}
---
+++ FUNdamentals/FUNdamentals/RedBlackBST.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeQuestions
+{
+    public class Node<T> where T : IComparable
+    {
+        public T value;
+        public Node<T> Left;
+        public Node<T> Right;
+        bool IsBlack;
+    }
+
+    class RedBlackBST<T> where T : IComparable
+    {
+        public void Insert(T value)
+        {
+
+        }
+    }
+}
