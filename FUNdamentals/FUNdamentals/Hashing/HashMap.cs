12bf9e401f6cb935552ac076f207c45e413dc0eb
Chase Violet <ccviol01@gmail.com>
2017-03-25 14:15:12 -0600

    Some hashing

---
+++ FUNdamentals/FUNdamentals/Hashing/HashMap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNdamentals.Hashing
+{
+
+    /// <summary>
+    /// Let's start basic... Initialize buckets at size 100
+    /// </summary>
+    class HashMap<Key, Value>
+    {
+        int[] Buckets;
+
+        public HashMap()
+        {
+            Buckets = new int[100];
+            for(int i=0; i<100; i++)
+            {
+                Buckets[i] = i;
+            }
+        }
+
+
+        public void Insert(Key key, Value value)
+        {
+            int hashCode = key.GetHashCode() & 0x7FFFFFFF;
+            int targetBucket = hashCode % Buckets.Length;
+        }
+    }
+}
